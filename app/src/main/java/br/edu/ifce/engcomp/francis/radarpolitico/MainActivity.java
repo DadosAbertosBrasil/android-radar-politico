419e530e65362d6ad7ada76c13cbd8aa0b0a0da6
Francisco José A. C. Souza <francisceioseph@gmail.com>
2016-03-10 14:59:06 -0300

    - Feature: Criação de Tabhost - Feature: Criação de Fragments de Politicos e votações - Feature: Cores do Aplicativo

--- app/src/main/java/br/edu/ifce/engcomp/francis/radarpolitico/MainActivity.java
+++ app/src/main/java/br/edu/ifce/engcomp/francis/radarpolitico/MainActivity.java
@@ -1,13 +1,18 @@
 package br.edu.ifce.engcomp.francis.radarpolitico;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,17 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initToolbar();
+        initTabHost();
     }
 
     @Override
@@ -44,5 +40,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void initTabHost(){
+        FragmentTabHost mainTabHost = (FragmentTabHost) findViewById(R.id.main_tabHost);
+        mainTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        TabHost.TabSpec votacoesTab  = mainTabHost.newTabSpec("votacoes");
+        TabHost.TabSpec politicosTab = mainTabHost.newTabSpec("politicos");
+
+        votacoesTab.setIndicator("VOTAÇÕES");
+        politicosTab.setIndicator("POLÍTICOS");
+
+        mainTabHost.addTab(votacoesTab, VotacoesFragment.class, null);
+        mainTabHost.addTab(politicosTab, PoliticosFragment.class, null);
+
+        mainTabHost.setCurrentTab(0);
+
+        stylizeTabs(mainTabHost);
+    }
+
+    private void stylizeTabs(FragmentTabHost tabHost){
+        ColorStateList tabTextColors;
+        TabWidget tabWidget;
+        TextView tabTextView;
+        View tabView;
+
+        int tabAmount;
+
+        tabWidget     = tabHost.getTabWidget();
+        tabTextColors = this.getResources().getColorStateList(R.color.tab_text_selector);
+        tabAmount     = tabWidget.getTabCount();
+
+        for (int i = 0; i < tabAmount; i++){
+            tabView = tabWidget.getChildTabViewAt(i);
+            //tabView.setBackgroundResource(R.drawable.tab_indicator);
+
+            tabTextView = (TextView) tabView.findViewById(android.R.id.title);
+            tabTextView.setTextColor(tabTextColors);
+        }
     }
 }
---
+++ app/src/main/java/br/edu/ifce/engcomp/francis/radarpolitico/VotacoesFragment.java
@@ -0,0 +1,24 @@
+package br.edu.ifce.engcomp.francis.radarpolitico;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class VotacoesFragment extends Fragment {
+    
+    public VotacoesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_votacoes, container, false);
+    }
+
+}
