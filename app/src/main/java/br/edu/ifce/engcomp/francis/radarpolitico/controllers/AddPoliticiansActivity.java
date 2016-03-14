588a9d87ac1180e24fe9a1f9527df7763ab18b8e
Joamila Brito <joamila.brito@gmail.com>
2016-03-14 15:32:07 -0300

    -Criação de activity adicionar deputados.

---
+++ app/src/main/java/br/edu/ifce/engcomp/francis/radarpolitico/controllers/AddPoliticiansActivity.java
@@ -0,0 +1,23 @@
+package br.edu.ifce.engcomp.francis.radarpolitico.controllers;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import br.edu.ifce.engcomp.francis.radarpolitico.R;
+
+public class AddPoliticiansActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_politicians);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
