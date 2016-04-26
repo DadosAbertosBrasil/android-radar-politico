e4029ef66fa70695c11f94799cf903f2eb955db5
Francisco A C Souza <francisceioseph@gmail.com>
2016-04-26 15:19:28 -0300

    - Features: Ordenar deputados da tela de AddDeputadoActivity por nome parlamentar - Feature: Atualizar fragment de politicos quando um novo politico é adicionado
    - Features: Carregar faltas de um deputado
    
    - Features: Carregar total de faltas de um deputado no ,mês

--- app/src/main/kotlin/br/edu/ifce/engcomp/francis/radarpolitico/miscellaneous/adapters/DeputadoRecyclerViewAdapter.kt
+++ app/src/main/kotlin/br/edu/ifce/engcomp/francis/radarpolitico/miscellaneous/adapters/DeputadoRecyclerViewAdapter.kt
@@ -3,6 +3,7 @@ package br.edu.ifce.engcomp.francis.radarpolitico.miscellaneous.adapters
 import android.content.Context
 import android.content.Intent
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,17 +13,23 @@ import android.widget.TextView
 
 import org.apache.commons.lang3.text.WordUtils
 
-import java.util.ArrayList
-
 import br.edu.ifce.engcomp.francis.radarpolitico.R
 import br.edu.ifce.engcomp.francis.radarpolitico.controllers.DeputadoActivity
+import br.edu.ifce.engcomp.francis.radarpolitico.helpers.VolleySharedQueue
+import br.edu.ifce.engcomp.francis.radarpolitico.miscellaneous.CDUrlFormatter
+import br.edu.ifce.engcomp.francis.radarpolitico.miscellaneous.connection.parsers.FrequenciaParser
 import br.edu.ifce.engcomp.francis.radarpolitico.models.Deputado
+import com.android.volley.Request
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
 import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Created by francisco on 11/03/16.
  */
-class DeputadoRecyclerViewAdapter(private val dataSource: ArrayList<Deputado>) : RecyclerView.Adapter<DeputadoRecyclerViewAdapter.ViewHolder>() {
+class DeputadoRecyclerViewAdapter(val context: Context, private val dataSource: ArrayList<Deputado>) : RecyclerView.Adapter<DeputadoRecyclerViewAdapter.ViewHolder>() {
 
     fun getItem(position: Int): Deputado {
         return this.dataSource[position]
@@ -35,15 +42,6 @@ class DeputadoRecyclerViewAdapter(private val dataSource: ArrayList<Deputado>) :
         return ViewHolder(itemView)
     }
 
-    fun ImageView.loadImage(url: String?){
-        if (!url.isNullOrBlank()) {
-            Picasso.with(this.context).load(url).into(this)
-        }
-        else {
-            this.setImageDrawable(resources.getDrawable(R.drawable.ic_smile))
-        }
-    }
-
     override fun onBindViewHolder(holder: DeputadoRecyclerViewAdapter.ViewHolder, position: Int) {
         val deputado = this.dataSource[position]
 
@@ -51,16 +49,48 @@ class DeputadoRecyclerViewAdapter(private val dataSource: ArrayList<Deputado>) :
         holder.partidoPoliticoTextView.text = deputado.partido
         holder.fotoPoliticoImageView.loadImage(deputado.urlFoto)
 
-        if (deputado.frequencia != null) {
-            val percentualFrequencia = deputado.frequencia!!.percentualPresenca
+        val currentCalendar  = Calendar.getInstance()
+        val firstDayCalendar = Calendar.getInstance()
+        val formatter   = SimpleDateFormat("dd/MM/yyyy")
+
+        firstDayCalendar.set(Calendar.DAY_OF_MONTH, 1)
+
+        val dataInicio = formatter.format(firstDayCalendar.time)
+        val dataFim = formatter.format(currentCalendar.time)
+
+        val urlRequest = CDUrlFormatter.listarPresencasParlamentar(dataInicio, dataFim, deputado.matricula)
+        val request = StringRequest(Request.Method.GET, urlRequest, {
+            stringResponse: String ->
+
+            val dias = FrequenciaParser.parseFrequenciaFromXML(stringResponse.byteInputStream())
+            val diasPresente = dias.filter { it.frequencia!!.equals("Presença") }
+            val percentualFrequencia = (diasPresente.size.toFloat() / dias.size) * 100
+
             holder.frasePresencasTextView.text = String.format("%4.1f%% de Presença no mês atual", percentualFrequencia)
-            holder.presencaMensalProgressBar.progress = Math.round(percentualFrequencia).toInt()
-            holder.numeroVotacoesTextView.text = deputado.frequencia!!.numeroSessoes.toString()
-        }
+            holder.presencaMensalProgressBar.isIndeterminate = false
+            holder.presencaMensalProgressBar.progress = percentualFrequencia.toInt()
+            holder.numeroVotacoesTextView.text = diasPresente.size.toString()
+
+
+        }, {
+            volleyError: VolleyError ->
+            volleyError.printStackTrace()
+        })
+
+        VolleySharedQueue.getQueue(context)?.add(request)
     }
 
     override fun getItemCount(): Int {
         return this.dataSource.size
+    }
+
+    fun ImageView.loadImage(url: String?){
+        if (!url.isNullOrBlank()) {
+            Picasso.with(this.context).load(url).into(this)
+        }
+        else {
+            this.setImageDrawable(resources.getDrawable(R.drawable.ic_smile))
+        }
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
