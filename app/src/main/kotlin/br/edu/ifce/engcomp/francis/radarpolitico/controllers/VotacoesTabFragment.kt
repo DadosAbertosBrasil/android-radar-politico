6b189620ab8dde539b3640735fefd5f806078d8d
Francisco A. C. Souza <francisceioseph@gmail.com>
2016-04-29 11:22:11 -0300

    - Feature: Add refresh functionality to VotaçõesTabFragment

--- app/src/main/kotlin/br/edu/ifce/engcomp/francis/radarpolitico/controllers/VotacoesTabFragment.kt
+++ app/src/main/kotlin/br/edu/ifce/engcomp/francis/radarpolitico/controllers/VotacoesTabFragment.kt
@@ -3,6 +3,7 @@ package br.edu.ifce.engcomp.francis.radarpolitico.controllers
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -20,12 +21,14 @@ import br.edu.ifce.engcomp.francis.radarpolitico.models.Proposicao
 import com.android.volley.Request
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.StringRequest
+import kotlinx.android.synthetic.main.content_add_politicians.*
 import java.util.*
 
-class VotacoesTabFragment : Fragment() {
+class VotacoesTabFragment : Fragment() , SwipeRefreshLayout.OnRefreshListener{
     lateinit var adapter: ProposicoesVotadasRecyclerViewAdapter
     lateinit var votacoesRecyclerView: RecyclerView
 
+    internal var swipeRefreshLayout: SwipeRefreshLayout? = null
     internal var votacoesProgressBar: ProgressBar? = null
     internal var datasource: ArrayList<Proposicao>
 
@@ -44,17 +47,25 @@ class VotacoesTabFragment : Fragment() {
         val rootView = inflater!!.inflate(R.layout.fragment_votacoes, container, false)
 
         this.votacoesRecyclerView = rootView.findViewById(R.id.votacoesRecyclerView) as RecyclerView
-        this.votacoesProgressBar  = rootView.findViewById(R.id.votacoesProgressBar) as ProgressBar
+        this.votacoesProgressBar  = rootView.findViewById(R.id.votacoesProgressBar)  as ProgressBar
+        this.swipeRefreshLayout   = rootView.findViewById(R.id.swipeRefreshLayout)   as SwipeRefreshLayout
 
         if(datasource.size > 0) {
             votacoesProgressBar?.visibility = View.INVISIBLE
         }
 
         this.initRecyclerView()
+        this.initSwipeRefreshLayout()
 
         return rootView
     }
 
+    override fun onRefresh() {
+        this.datasource.clear()
+        this.adapter.notifyDataSetChanged()
+
+        this.retrieveVotedPropositionsOfCurrentYearFromServer()
+    }
     private fun initRecyclerView() {
         val layoutManager = LinearLayoutManager(activity)
 
@@ -62,6 +73,11 @@ class VotacoesTabFragment : Fragment() {
         this.votacoesRecyclerView.layoutManager = layoutManager
         this.votacoesRecyclerView.adapter = adapter
         this.votacoesRecyclerView.itemAnimator = DefaultItemAnimator()
+    }
+
+    private fun initSwipeRefreshLayout(){
+        swipeRefreshLayout?.setOnRefreshListener(this)
+        swipeRefreshLayout?.setColorSchemeColors(R.color.colorPrimaryDark, R.color.colorPrimary, R.color.colorAccent)
     }
 
     private fun retrieveVotedPropositionsOfCurrentYearFromServer() {
@@ -79,10 +95,14 @@ class VotacoesTabFragment : Fragment() {
             adapter.notifyDataSetChanged()
 
             this.votacoesProgressBar?.visibility = View.INVISIBLE
+            this.swipeRefreshLayout?.isRefreshing = false
 
         }, {
             volleyError: VolleyError ->
             Toast.makeText(activity, "Ocorreu algum erro de rede. Tente mais tarde. :/", Toast.LENGTH_SHORT).show()
+
+            this.votacoesProgressBar?.visibility = View.INVISIBLE
+            this.swipeRefreshLayout?.isRefreshing = false
         })
 
         VolleySharedQueue.getQueue(activity)?.add(request)
