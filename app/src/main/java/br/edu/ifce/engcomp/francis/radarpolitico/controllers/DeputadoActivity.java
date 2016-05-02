7fdb92178e077fab4588f55efe5a419a98ceeb09
Joamila <joamila.brito@gmail.com>
2016-05-02 15:19:54 -0300

    -Deputado: carregar votações do mês (ainda não terminado).

--- app/src/main/java/br/edu/ifce/engcomp/francis/radarpolitico/controllers/DeputadoActivity.java
+++ app/src/main/java/br/edu/ifce/engcomp/francis/radarpolitico/controllers/DeputadoActivity.java
@@ -20,6 +20,7 @@ import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.RequestFuture;
 import com.android.volley.toolbox.StringRequest;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
@@ -40,10 +41,15 @@ import br.edu.ifce.engcomp.francis.radarpolitico.miscellaneous.CDUrlFormatter;
 import br.edu.ifce.engcomp.francis.radarpolitico.miscellaneous.adapters.DeputadoFaltasRecyclerViewAdapter;
 import br.edu.ifce.engcomp.francis.radarpolitico.miscellaneous.adapters.DeputadoVotacoesRecyclerViewAdapter;
 import br.edu.ifce.engcomp.francis.radarpolitico.miscellaneous.connection.parsers.FrequenciaParser;
+import br.edu.ifce.engcomp.francis.radarpolitico.miscellaneous.connection.parsers.ProposicaoParser;
+import br.edu.ifce.engcomp.francis.radarpolitico.miscellaneous.connection.parsers.ProposicoesParser;
+import br.edu.ifce.engcomp.francis.radarpolitico.miscellaneous.connection.parsers.VotacaoParser;
 import br.edu.ifce.engcomp.francis.radarpolitico.models.Deputado;
 import br.edu.ifce.engcomp.francis.radarpolitico.models.Dia;
 import br.edu.ifce.engcomp.francis.radarpolitico.models.Falta;
 import br.edu.ifce.engcomp.francis.radarpolitico.models.Frequencia;
+import br.edu.ifce.engcomp.francis.radarpolitico.models.Proposicao;
+import br.edu.ifce.engcomp.francis.radarpolitico.models.Votacao;
 import br.edu.ifce.engcomp.francis.radarpolitico.models.VotacaoDeputado;
 import br.edu.ifce.engcomp.francis.radarpolitico.models.Voto;
 
@@ -56,6 +62,8 @@ public class DeputadoActivity extends AppCompatActivity {
     ArrayList<Falta> faltas = new ArrayList<>();
     ProgressDialog progressDialog;
     DeputadoFaltasRecyclerViewAdapter adapterFaltas;
+    DeputadoVotacoesRecyclerViewAdapter adapterVotacoes;
+    ArrayList<Proposicao> proposicoes = new ArrayList<>();
 
     public DeputadoActivity(){
 
@@ -74,13 +82,11 @@ public class DeputadoActivity extends AppCompatActivity {
         String dataIni = "01/04/2016";
         String dataFin = "30/04/2016";
 
-
         String urlRequest = CDUrlFormatter.listarPresencasParlamentar(dataInicio, dataFim, deputado.getMatricula());
         StringRequest request = new StringRequest(Request.Method.GET, urlRequest, new Response.Listener<String>() {
 
             @Override
             public void onResponse(String response) {
-                Log.i("RESPONSE", response);
                 try {
                     ArrayList<Dia> dias = FrequenciaParser.parseFrequenciaFromXML(new ByteArrayInputStream(response.getBytes()));
                     if(dias!=null){
@@ -102,7 +108,6 @@ public class DeputadoActivity extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.i("RESPONSE", "Erro no servidor!");
                 Toast.makeText(getApplicationContext(), "Erro no servidor!", Toast.LENGTH_SHORT).show();
             }
         });
@@ -112,15 +117,45 @@ public class DeputadoActivity extends AppCompatActivity {
     }
 
     public ArrayList<VotacaoDeputado> generateDataSourceVotacoes(){
-        ArrayList<VotacaoDeputado> votacoes = new ArrayList<>();
+        final ArrayList<VotacaoDeputado> votacoes = new ArrayList<>();
 
-        VotacaoDeputado teste1 = new VotacaoDeputado("PEC00/1969", "02/02/2016", "SIM");
-        VotacaoDeputado teste2 = new VotacaoDeputado("PL99/1932", "15/02/2016", "SIM");
-        VotacaoDeputado teste3 = new VotacaoDeputado("ADV24/1944", "12/03/2016", "NÃO");
+        Calendar currentCalendar  = Calendar.getInstance();
+        Calendar firstDayCalendar = Calendar.getInstance();
+        SimpleDateFormat formatter   = new SimpleDateFormat("dd/MM/yyyy");
 
-        votacoes.add(teste1);
-        votacoes.add(teste2);
-        votacoes.add(teste3);
+        firstDayCalendar.set(Calendar.DAY_OF_MONTH, 1);
+
+        String dataInicio = formatter.format(firstDayCalendar.getTime());
+        String dataFim = formatter.format(currentCalendar.getTime());
+        String dataIni = "27/04/2016";
+        String dataFin = "28/04/2016";
+
+        String urlRequestProp = CDUrlFormatter.listarProposicoesTramitadasNoPeriodo(dataIni, dataFin);
+        StringRequest requestP = new StringRequest(Request.Method.GET, urlRequestProp, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                try {
+                    proposicoes = ProposicoesParser.parseProposicoesFromXML(
+                            new ByteArrayInputStream(response.getBytes()));
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                }
+
+                adapterVotacoes.notifyDataSetChanged();
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Erro no servidor!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        VolleySharedQueue.INSTANCE.getQueue(this).add(requestP);
 
         return votacoes;
     }
@@ -142,6 +177,7 @@ public class DeputadoActivity extends AppCompatActivity {
         }
 
         init();
+        this.datasourceVotacoes = generateDataSourceVotacoes();
         this.datasourceFaltas = generateDataSourceFaltas();
         initRecyclerViewFaltas();
         initRecyclerViewVotacoes();
@@ -188,11 +224,11 @@ public class DeputadoActivity extends AppCompatActivity {
 
     public void initRecyclerViewVotacoes(){
         LinearLayoutManager layoutManager   = new LinearLayoutManager(this);
-        DeputadoVotacoesRecyclerViewAdapter adapter = new DeputadoVotacoesRecyclerViewAdapter(this.datasourceVotacoes, this);
+        adapterVotacoes = new DeputadoVotacoesRecyclerViewAdapter(this.datasourceVotacoes, this);
 
         this.recyclerViewVotacoes.setHasFixedSize(false);
         this.recyclerViewVotacoes.setLayoutManager(layoutManager);
-        this.recyclerViewVotacoes.setAdapter(adapter);
+        this.recyclerViewVotacoes.setAdapter(adapterVotacoes);
         this.recyclerViewVotacoes.setItemAnimator(new DefaultItemAnimator());
     }
 }
